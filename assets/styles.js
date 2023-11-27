d5c58e9a8b75fb061292250259bc9680fa46305d
Yefenny Chireno <yefenny300@icloud.com>
2023-11-27 13:49:40 -0500

    mobile home is complete with menu toggling

--- assets/styles.js
+++ assets/styles.js
@@ -1,5 +1,24 @@
 // Close navbar on mobile
-function closeBar(){
-   var element = document.getElementById("nav-bar");
-   element.classList.remove("show-bar");
+function closeBar() {
+  var element = document.getElementById('nav-bar');
+  if (element) {
+    let isOpen = element.classList.contains('show-bar');
+    if (isOpen) {
+      element.classList.remove('show-bar');
+    } else {
+      element.classList.add('show-bar');
+    }
+    hidden();
+  }
+
+  function hidden() {
+    var element = document.getElementById('home-content');
+    let ishidden = element.getAttribute('hidden');
+
+    if (ishidden) {
+      element.removeAttribute('hidden');
+    } else {
+      element.setAttribute('hidden', 'hidden');
+    }
+  }
 }
